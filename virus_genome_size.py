244d09979438122b9817ab23cc6a607e4aaa9fb9
Matt Gitzendanner <magitz@ufl.edu>
2019-03-01 10:58:33 -0500

    Add CDS count

--- virus_genome_size.py
+++ virus_genome_size.py
@@ -17,6 +17,22 @@ import os
 # Version 1.0: Initial version 
 # =====================================================
 
+# Function based on https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/genbank/
+def index_genbank_features(gb_record, feature_type, qualifier) :
+    feature_count= dict()
+    for (index, feature) in enumerate(gb_record.features) :
+        if feature.type==feature_type :
+            if qualifier in feature.qualifiers :
+                #There should only be one locus_tag per feature, but there
+                #are usually several db_xref entries
+                for value in feature.qualifiers[qualifier] :
+                    if value in answer :
+                        print "WARNING - Duplicate key %s for %s features %i and %i" \
+                           % (value, feature_type, feature_count[value], index)
+                    else :
+                        feature_count[value] = index
+    return feature_count
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="Filename of GenBank formatte file to process")
@@ -24,4 +40,6 @@ args = parser.parse_args()
 
 
 for seq_record in SeqIO.parse(args.file, "genbank"):
-    print(seq_record )
+    CDS_count = index_genbank_features(seq_record.id, "CDS", "gene")
+    print(seq_record.id, len(seq_record.seq), CDS_count )
+
