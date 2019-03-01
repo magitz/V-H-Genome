40d515c45f7186ed7be511689fda10a92b30e81f
Matt Gitzendanner <magitz@ufl.edu>
2019-03-01 11:09:21 -0500

    Length of dict

--- virus_genome_size.py
+++ virus_genome_size.py
@@ -19,19 +19,19 @@ import os
 
 # Function based on https://warwick.ac.uk/fac/sci/moac/people/students/peter_cock/python/genbank/
 def index_genbank_features(gb_record, feature_type, qualifier) :
-    feature_count= dict()
+    feature_dict= dict()
     for (index, feature) in enumerate(gb_record.features) :
         if feature.type==feature_type :
             if qualifier in feature.qualifiers :
                 #There should only be one locus_tag per feature, but there
                 #are usually several db_xref entries
                 for value in feature.qualifiers[qualifier] :
-                    if value in feature_count :
+                    if value in feature_dict :
                         print ("WARNING - Duplicate key %s for %s features %i and %i" \
-                           % (value, feature_type, feature_count[value], index))
+                           % (value, feature_type, feature_dict[value], index))
                     else :
-                        feature_count[value] = index
-    return feature_count
+                        feature_dict[value] = index
+    return feature_dict
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -40,7 +40,7 @@ args = parser.parse_args()
 
 
 for seq_record in SeqIO.parse(args.file, "genbank"):
-    CDS_count = index_genbank_features(seq_record, "CDS", "gene")
+    CDS_count = len( index_genbank_features(seq_record, "CDS", "gene") )
     print(seq_record.id, len(seq_record.seq), CDS_count )
     break #Let's process one for testing.
 
